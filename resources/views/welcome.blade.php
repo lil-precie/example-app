3c25f598b220e3d9e7391285707cbba72d8739ac
lil-precie <preciousede436@gmail.com>
2023-09-25 15:56:16 +0100

    welcome page adjusted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    me
+                    testing
                 </div>
 
                 <div class="links">
